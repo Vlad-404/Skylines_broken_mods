e9ebc494b545effec10fdc49c563a8a1c073f311
Vlad-404 <vmijat21@gmail.com>
2022-01-31 19:05:52 +0100

    Add index, script and css files

---
+++ script.js
@@ -0,0 +1,8 @@
+// const fs = require("fs");
+// csv = fs.readFileSync("CS_broken-mods.csv")
+
+// let csvFile = ./CS_broken-mods.csv
+// console.log(csvFile);
+
+const csvFile = document.getElementById('csv-file')
+console.log(csvFile)
