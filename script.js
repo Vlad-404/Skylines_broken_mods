551cc99027784a8f8ca0f18b1d36d2fac5b951f8
Vlad-404 <vmijat21@gmail.com>
2022-01-31 19:25:10 +0100

    Add broken_mods.json test file

--- script.js
+++
@@ -1,8 +0,0 @@
-// const fs = require("fs");
-// csv = fs.readFileSync("CS_broken-mods.csv")
-
-// let csvFile = ./CS_broken-mods.csv
-// console.log(csvFile);
-
-const csvFile = document.getElementById('csv-file')
-console.log(csvFile)
