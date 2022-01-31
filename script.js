ff4722eb329379d7be079d0f0b0959007dbdd993
Vlad-404 <vmijat21@gmail.com>
2022-01-31 22:22:56 +0100

    Changed the structure of json file and add display of mods in html with JS

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
+const brokenModsTable = document.getElementById('mods-list')
+
+let brokenModsArray = []
+
+let showBrokenMods = (brokenModsList) => {
+    brokenModsTable.innerHTML = `
+        <tr>
+            <th>Mod</th>
+            <th>SteamID</th>
+            <th>URL</th>
+            <th>Issue</th>
+            <th>Replacement</th>
+            <th>Replacement Option 1</th>
+            <th>Replacement Option 2</th>
+        </tr>
+    `
+
+    brokenModsList.forEach((mod) => {
+        const { steamID, title, url, issue, replacements } = mod
+        console.log(mod)
+
+        const modEl = document.createElement('tr')
+        modEl.classList.add('mod-row')
+        brokenModsTable.appendChild(modEl)
+
+        modEl.innerHTML = `
+            <!-- Name of the mod -->
+            <td style="font-weight: bold;">${title}</td>
+            <!-- SteamID -->
+            <td>${steamID}</td>
+            <!-- URL on Steam workshop-->
+            <td>${url}</td>
+            <!-- Issue -->
+            <td>${issue}</td>
+            <td>Replace it with:</td>
+            <!-- Replace it with -->
+            <td><a href="${replacements.replacement1.url}" target="_blank">${replacements.replacement1.title}</a></td>
+        `
+    });
+}
+
 fetch('./broken_mods.json')
 .then(response => {
     return response.json();
 })
-.then (jsondata => console.log(jsondata, jsondata.ARIS_Early_Death.replacements.replacement1.title));
+.then (jsondata => showBrokenMods(jsondata));
