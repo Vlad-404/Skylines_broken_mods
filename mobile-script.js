1688a099a341a6d91df350848bc989fb2262913a
Vlad-404 <vmijat21@gmail.com>
2022-03-08 15:25:51 +0100

    Replace the table template and add switch for desktop site

--- mobile-script.js
+++ mobile-script.js
@@ -1,6 +1,11 @@
-fetch('./broken_mods.json')
-.then(response => {
-    return response.json();
-})
-//.then (jsondata => showBrokenMods(jsondata));
-.then (jsondata => console.log(jsondata));
+// fetch('./broken_mods.json')
+// .then(response => {
+//     return response.json();
+// })
+// //.then (jsondata => showBrokenMods(jsondata));
+// .then (jsondata => console.log(jsondata));
+
+// redirect to desktop site - temp
+if (screen.width > 768) {
+    document.location = "./index.html";
+    }
