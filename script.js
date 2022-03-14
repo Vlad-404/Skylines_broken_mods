f620ddd7c8b05b6a80079891e547f12111d594a2
Vlad-404 <vmijat21@gmail.com>
2022-03-14 18:02:43 +0100

    Add edit and remove buttons for mods

--- script.js
+++ script.js
@@ -23,14 +23,16 @@ let showBrokenMods = (brokenModsList) => {
         brokenModsTable.appendChild(modEl)
 
         modEl.innerHTML = `
+        <button class="edit edit-sizes edit-icon"><i class="fas fa-edit icon"></i></button>
         <div class="mod-row bold w-20">${title}</div>
-        <div class="mod-row w-10">${steamID}</div>
-        <div class="mod-row align-text-mid w-10"><a href="${url}" target="_blank" rel="noopener"><i class="fab fa-steam"></i></a></div>
+        <div class="mod-row text-mid w-10">${steamID}</div>
+        <div class="mod-row text-mid w-10"><a href="${url}" target="_blank" rel="noopener"><i class="fab fa-steam"></i></a></div>
         <div class="mod-row w-20">${issue}</div>
         <div class="mod-row w-15"><a href="${replacements.replacement1.url}" target="_blank" rel="noopener">${replacements.replacement1.title}</a></div>
         <div class="mod-row w-15">
-            ${replacements.replacement2 ? `<a href="${replacements.replacement2.url}" target="_blank" rel="noopener">${replacements.replacement2.title}</a>` : '-'}
+            ${replacements.replacement2 ? `<a href="${replacements.replacement2.url}" target="_blank" rel="noopener">${replacements.replacement2.title}</a>` : ' '}
         </div>
+        <button class="edit edit-sizes del-icon"><i class="fas fa-trash-alt icon"></i></button>
         `
     });
 }
