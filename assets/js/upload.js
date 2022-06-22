9dc303cbaf3a7d98ba5dfe75e90f1da6c081e82a
Vlad-404 <vmijat21@gmail.com>
2022-06-22 18:30:46 +0200

    Add convert csv to json feature

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -41,23 +41,44 @@ const uploadedFile = document.getElementById('file')
 const uploadBtn = document.getElementById('upload-btn')
 
 // Reading local file with AJAX
-// let stringyfiedCsv = ''
-// let xmlhttp = new XMLHttpRequest();
-//     xmlhttp.onreadystatechange = function(){
-//     if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
-//         stringyfiedCsv = xmlhttp.responseText;
-//     }
-// };
-// xmlhttp.open("GET", "broken_mods.csv", true);
-// xmlhttp.send();
+let stringyfiedCsv = ''
+let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+    if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
+        stringyfiedCsv = xmlhttp.responseText;
+    }
+};
+xmlhttp.open("GET", "broken_mods.csv", true);
+xmlhttp.send();
 
-// console.log(stringyfiedCsv)
+console.log(stringyfiedCsv)
 
 uploadBtn.addEventListener('click', () => {
     if (uploadedFile.files[0] === undefined) {
         console.log('No file!')
     } else {
         console.log(uploadedFile.files[0])
+
+        const CSVToJSON = (csv) => {
+            const lines = csv.split('\n');
+            const keys = lines[0].split(',');
+            return lines.slice(1).map(line => {
+                return line.split(',').reduce((acc, cur, i) => {
+                    const toAdd = {};
+                    toAdd[keys[i]] = cur;
+                    return { ...acc, ...toAdd };
+                }, {});
+            });
+        };
+
+        console.log(CSVToJSON(stringyfiedCsv))
+
+        // function logFile(event) {
+        //     let str = event.target.result;
+        //     let json = JSON.parse(str);
+        //     console.log('string', str);
+        //     console.log('json', json);
+        // }
         // const json = CSVJSON.csv2json(uploadedFile.files[0], {parseNumbers: true});
         // let stringyCsv = uploadedFile.files[0].toString()
         // console.log(stringyCsv)
